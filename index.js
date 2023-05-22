dfaf728d72e3125011633199ac4ca47092f170ab
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-22 19:53:36 +0300

    UPDATE: checking function invokeAction

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const contacts = require('constants')
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactsList = await listContacts();
+      console.table(contactsList);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "rsKkOQUi80UsgVP" });
+
+// invokeAction({
+//   action: "add",
+//   name: "Yulia Rudenko",
+//   email: "yulyarudenko88@gmail.com",
+//   phone: "(050) 699-29-18",
+// });
+
+// invokeAction({ action: "remove", id: "rsKkOQUi80UsgVPCcLZZW" });
+// invokeAction({ action: "lis" });
