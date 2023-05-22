07ead42b7cb5d68ac82b6031f756640e67405a54
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-22 17:55:30 +0300

    CREATE: contacts.js

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
